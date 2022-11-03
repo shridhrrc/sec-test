200c307fd8081bd85df190488cbde5499a97688f
Shridhar Karigoudar <shree277004@gmail.com>
2022-11-03 17:24:41 +0530

    msg

---
+++ config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  username: 'shridhar@mysqlserversfor',
+  password: '$ucce$$@10484',
+  database: 'sectest',
+  host:'mysqlserversfor.mysql.database.azure.com',
+  port: 3306,
+  dialect: 'mysql'
+}
